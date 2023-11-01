481c325e2b00b7ac6777ce04feaf91891ea5f5f2
Isaac Rodríguez <isaac.rodriguez@vifaru.mx>
2023-11-01 11:37:14 -0700

    Add FliuenValidation nuget package and implement a validator for the CreateEmployee method

---
+++ DocSolutionsCodeChallenge/Validators/CreateEmployeeValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using DocSolutionsCodeChallenge.Models; // Assuming you have an Employee model class
+using DocSolutionsCodeChallenge.Repositories; // Import the namespace for IEmployeeRepository
+
+namespace DocSolutionsCodeChallenge.Validators
+{
+    public class CreateEmployeeValidator : AbstractValidator<Employee>
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public CreateEmployeeValidator(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+
+            RuleFor(employee => employee.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .NotNull().WithMessage("Name cannot be null.");
+
+            RuleFor(employee => employee.User)
+                .NotEmpty().WithMessage("User is required.")
+                .NotNull().WithMessage("User cannot be null.");
+
+            RuleFor(employee => employee.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .NotNull().WithMessage("Password cannot be null.");
+
+            RuleFor(employee => employee.User)
+                .Must(user => !UserExists(user)).WithMessage("User already exists.");
+        }
+
+        private bool UserExists(string user)
+        {
+            var employee = _employeeRepository.UserEmployeeExist(user);
+            return employee != null;
+        }
+    }
+}
